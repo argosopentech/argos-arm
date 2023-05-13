ceebdfcd22931324470c34699dd18cb904cbddf9
P.J. Finlay <pjappdevelopment@gmail.com>
2023-05-13 14:03:56 -0500

    Update JSONIPC

--- argosarm/jsonipc.py
+++ argosarm/jsonipc.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import multiprocessing
 import os
 import pathlib
@@ -7,31 +8,30 @@ import pathlib
 class Pipe:
     # Opening a FIFO pipe will block until both a reader and a writer are available
     def __init__(self, path: pathlib.Path):
-        self.lock = multiprocessing.Lock()
         self.path = path
         self.listeners = list()
         os.mkfifo(self.path, mode=0o660)
 
     def add_listener(self, listener: callable):
-        with self.lock:
-            self.listeners.append(listener)
+        self.listeners.append(listener)
 
     def listen(self):
         # Open FIFO pipe file with line buffering
         f = open(self.path, "r", buffering=1)
+        logging.info(f"Listening on pipe {self.path}")
 
         while True:
-            with self.lock:
-                line = f.read()
-                if len(line) == 0:
-                    continue
-                o = json.loads(line)
-                for listener in self.listeners:
-                    listener(o)
+            line = f.read()
+            if len(line) == 0:
+                continue
+            o = json.loads(line)
+            for listener in self.listeners:
+                listener(o)
 
         f.close()
 
     def write(self, data: str):
         with open(self.path, "w") as f:
+            logging.info(f"Writing to pipe {self.path}")
             data_str = json.dumps(data)
             f.write(data_str + "\n")
